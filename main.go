45bc79f81afdc87a233c50558319f26173d70a8d
rootspyro <root.spyro@gmail.com>
2024-02-28 02:58:46 -0400

    Feat: Add building of work data

--- main.go
+++ main.go
@@ -13,11 +13,12 @@ func main() {
 	resumeData := GetJsonData(jsonPath)
 
 	BuildDocument(
-		HeaderSection(resumeData),
+		BuildBasics(resumeData),
+		BuildWork(resumeData),	
 	)
 }
 
-func BuildDocument(header string) {
+func BuildDocument(header, work string) {
 	var LaTeXCode string
 
 	LaTeXCode = `
@@ -45,30 +46,96 @@ func BuildDocument(header string) {
 		`
 \begin{document}
 %s
+%s
 \end{document}
 		`,
 		header,
+		work,
 	)
 
 	fmt.Println(LaTeXCode)
 }
 
 // This function generate the LaTeX code of the Basics Section
-func HeaderSection(data JsonResume) string {
+func BuildBasics(data JsonResume) string {
+
 	var str string
 
+	// 
+	
 	str += fmt.Sprintf(
 		`
 \begin{flushleft}
     \textbf{\LARGE %s} \\
     %s \\
-    \href{https://johndoe.com}{johndoe.com} | john@gmail.com | (912) 555-4321 \\
+    \href{%s}{%s} | %s | %s \\
     2712 Broadway St, San Francisco, California, CA 94115, US
 \end{flushleft}
+\section*{Summary}
+%s\\
 		`,
 		data.Basics.Name,
 		data.Basics.Label,
+		data.Basics.Url,
+		data.Basics.Url,
+		data.Basics.Email,
+		data.Basics.Phone,
+		data.Basics.Summary,
 	)
+
+	str += `
+	`
+	return str
+} 
+
+func BuildWork(data JsonResume) string {
+
+	var str string
+
+	if len(data.Work) > 0 {
+
+		str = `
+\section*{Work Experience}
+\begin{itemize}[leftmargin=*]
+		`
+
+		for _, work := range data.Work {
+			str += fmt.Sprintf(
+				`
+  \item \entry{%s}{%s} \hfill %s - %s \\
+	%s \\
+	Highlights:
+	\begin{itemize}
+				`,
+				work.Position,
+				work.CompanyName,
+				work.StartDate,
+				work.EndDate,
+				work.Summary,
+			)
+
+			// Print the list of highlights
+			for _, highlight := range work.Highlights {
+				str += fmt.Sprintf(
+					`
+		\item %s				
+					`,
+					highlight,
+				)
+			}
+
+			str += `
+	\end{itemize}
+			`
+		}
+
+		// end of the section
+		str += `
+\end{itemize}
+		`
+		
+	}
+
 	return str
 }
 
